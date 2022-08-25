80dc04b3abc869fa18c4e907040efaa9e8dfe92b
MathiasSchjoedt-Bavngaard <81029612+MathiasSchjoedt-Bavngaard@users.noreply.github.com>
2022-08-25 14:25:39 +0200

    Add project files.

---
+++ MatchingStuff/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchingStuff
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartUpGame();
+
+        }
+
+        private void StartUpGame()
+        {
+            List<string> icons = new List<string>()
+            {
+                "🍕","🍕",
+                "🍔","🍔",
+                "🍟","🍟",
+                "🍗","🍗",
+                "🥗","🥗",
+                "🍤","🍤",
+                "🥨","🥨",
+                "🥓","🥓",
+
+
+            };
+            Random random = new Random();
+            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index =random.Next(icons.Count);
+                string nextIcon = icons[index];
+                textBlock.Text = nextIcon;
+                icons.RemoveAt(index);
+
+            }
+        }
+
+    }
+}
